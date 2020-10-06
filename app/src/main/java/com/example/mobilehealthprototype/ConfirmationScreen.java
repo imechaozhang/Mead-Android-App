e1805385ed8e60c476a8b9d65092fb1023d92c97
imechaozhang <chaozhang@uchicago.edu>
2020-10-05 23:22:29 -0500

    add google forms

--- app/src/main/java/com/example/mobilehealthprototype/ConfirmationScreen.java
+++ app/src/main/java/com/example/mobilehealthprototype/ConfirmationScreen.java
@@ -1,16 +1,21 @@
 package com.example.mobilehealthprototype;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.SmsManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.ajithvgiri.searchdialog.SearchListItem;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -20,28 +25,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 
-import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
 
 public class ConfirmationScreen extends AppCompatActivity {
+    private static final MediaType FORM_DATA_TYPE = MediaType.parse("application/x-www-form-urlencoded; charset=utf-8");
     Intent passedIntent;
     Sex p_sex;
     int p_id, p_age;
@@ -93,9 +86,14 @@ public class ConfirmationScreen extends AppCompatActivity {
                 // sendMessage(getString(R.string.server_number),toSend); //Check if this is working later
 
                 //send googleform
+                PostDataTask postDataTask = new PostDataTask();
 
-                saveFile(toSend);
-                readFile();
+                //execute asynctask
+                postDataTask.execute(toSend);
+
+
+                //saveFile(toSend);
+                //readFile();
 
 
                 Intent intent = new Intent(ConfirmationScreen.this, MainActivity.class);
@@ -188,44 +186,49 @@ public class ConfirmationScreen extends AppCompatActivity {
         sm.sendTextMessage(phone_num, null, msg, null, null);
     }
 
-    private void sendForm(String msg){
-        StringRequest request = new StringRequest(
-                Request.Method.POST,
-                "@string/google_form_url",
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("TAG", "Response: " + response);
-                        if (response.length() > 0) {
-                            Snackbar.make(fab, "Successfully Posted", Snackbar.LENGTH_LONG).show();
-                            edtName.setText(null);
-                            edtPhone.setText(null);
-                        } else {
-                            Snackbar.make(fab, "Try Again", Snackbar.LENGTH_LONG).show();
-                        }
-                        progressDialog.dismiss();
-                    }
-                }, new Response.ErrorListener() {
+    private class PostDataTask extends AsyncTask<String, Void, Boolean> {
 
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                progressDialog.dismiss();
-                Snackbar.make(fab, "Error while Posting Data", Snackbar.LENGTH_LONG).show();
+        @Override
+        protected Boolean doInBackground(String... data) {
+            String msg = data[0];
+            Boolean result = true;
+            // String url = "@strings/google_form_url";
+            String url = "https://docs.google.com/forms/d/e/1FAIpQLSe3Vuyr0WWWlc2MIKAq_aw8uYeQCdqg2dV_4B7TgcP5Aw2wpg/formResponse";
+            String record = "entry.519325928";
+            String feedback = "entry.1481931525";
+            String postBody = "";
+            try {
+                //all values must be URL encoded to make sure that special characters like & | ",etc.
+                //do not cause problems
+                postBody = record + "=" + URLEncoder.encode(msg, "UTF-8") +
+                        "&" + feedback + "=" + URLEncoder.encode("empty", "UTF-8");
+            } catch (UnsupportedEncodingException ex) {
+                result = false;
             }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put(Constants.nameField, name);
-                params.put(Constants.phoneField, phone);
-                return params;
+            try {
+                //Create OkHttpClient for sending request
+                OkHttpClient client = new OkHttpClient();
+                //Create the request body with the help of Media Type
+                RequestBody body = RequestBody.create(FORM_DATA_TYPE, postBody);
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(body)
+                        .build();
+                //Send the request
+                Response response = client.newCall(request).execute();
+            } catch (IOException exception) {
+                result = false;
             }
-        };
-        request.setRetryPolicy(new DefaultRetryPolicy(
-                50000,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-        queue.add(request);
+            System.out.println("===========");
+            System.out.println("sending result");
+            System.out.println(result);
+            System.out.println(msg);
+            System.out.println(postBody);
+            System.out.println("===========");
+
+
+            return result;
+        }
     }
 
 
